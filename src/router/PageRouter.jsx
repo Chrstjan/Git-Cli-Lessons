bac0d8c69ebcf4712572c73b83ed95fd55581e10
Christian <ps5christian@hotmail.com>
2024-10-11 10:30:22 +0200

    Ændret i landingpage text

--- src/router/PageRouter.jsx
+++ src/router/PageRouter.jsx
@@ -6,7 +6,7 @@ export const PageRouter = () => {
   return (
     <Routes>
       <Route path={Paths.home} element={<MainLayout />}>
-        <Route index element={<h1>React Boilerplate 2.0</h1>} />
+        <Route index element={<h1>Git Cli Branch Opgave</h1>} />
         <Route path={Paths.pageNotFound} element={<h2>404 not found...</h2>} />
       </Route>
     </Routes>
